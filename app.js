96520ad50f3a65f59064d147cada14c01c92a2c0
brain4ctivity <alexandru.alupei@stud.ubbcluj.ro>
2025-05-27 18:35:44 +0300

    1

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.use('/api/cpus', require('./routes/cpus'));
 app.use('/api/statistics', require('./routes/statistics'));
 app.use('/api/manufacturers', require('./routes/manufacturers'));
 app.use('/api/auth', require('./routes/auth'));
+app.use('/api/auth/complete-login', require('./routes/auth'));
 
 // Error handling middleware
 app.use((err, req, res, next) => {
